19e26ffb7519219eefd8b947234b30fa1ca7780e
undefined <44500896+Vandomas@users.noreply.github.com>
2023-03-20 22:24:42 +0300

    добавлено поисковое поле

--- src/server.js
+++ src/server.js
@@ -6,6 +6,7 @@ const express = require('express')
 const app = express();
 
 
+app.use(express.static(path.join(__dirname, 'frontend', 'assets')));
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'frontend', 'index.html'))
 });
