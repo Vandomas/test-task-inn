da49b7d694b4c672a07b6ef7054a2584519a4cc5
undefined <44500896+Vandomas@users.noreply.github.com>
2023-03-20 22:25:17 +0300

    убрал не нужный код

--- src/server.js
+++ src/server.js
@@ -6,7 +6,6 @@ const express = require('express')
 const app = express();
 
 
-app.use(express.static(path.join(__dirname, 'frontend', 'assets')));
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'frontend', 'index.html'))
 });
