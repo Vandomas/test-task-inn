80aa903ff3b731d65938396790bc489afad2441f
undefined <44500896+Vandomas@users.noreply.github.com>
2023-03-20 21:29:31 +0300

    setup project

---
+++ src/server.js
@@ -0,0 +1,16 @@
+require('dotenv').config('./.env');
+
+const path = require('path');
+
+const express = require('express')
+const app = express();
+
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'frontend', 'index.html'))
+});
+
+
+app.listen(process.env.SERVER_PORT, () => {
+    console.log(`Приложение работает на порту: ${process.env.SERVER_PORT}`)
+});
